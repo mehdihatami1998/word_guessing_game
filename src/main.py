c481e61e165de8067ca5a445660de00a42106dc9
Mehdi Hatami <mehdi.hatami@mail.polimi.it>
2023-09-30 22:50:19 +0200

    adding word_generator and importing the module

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from utils import word_generator
+
+
+def main():
+
+    word_generator.generate_word()
+    #print(f"selected word is {word_generator.word} from {word_generator.word} category")
+
+
+if __name__ == "__main__":
+    main()
