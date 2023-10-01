d11a7d2a4b99aeef71662bd6431b1e62e06c6ba7
Mehdi Hatami <mehdi.hatami@mail.polimi.it>
2023-10-01 20:52:56 +0200

    updating main.py  with game play for right guesses

--- src/main.py
+++ src/main.py
@@ -8,24 +8,57 @@ def main():
     selected_word = word_generator.generate_word()
     print(selected_word)
     print(f"you need to guess the right letters instead of underscores")
+
     blank_word = "_ " * len(selected_word)   
     print(f"{blank_word}")
 
+    number_of_guesses = len(selected_word)
+    print(f"you can have {number_of_guesses} wrong guesses")
+    wrong_guesses =""
 
     while True:
         
         user_input = input_validator.get_user_input()
         index = []
-        for i in range(len(selected_word)):
-            if user_input == selected_word[i]:
-                index.append(i)
-        for j in range(len(index)):
-            real_index = index[j]
-            blank_word = blank_word[:real_index*2] + user_input + blank_word[real_index*2 + 1:]
-        print(blank_word)
+    
+        if user_input in selected_word:
+            for i in range(len(selected_word)):
+                if user_input == selected_word[i]:
+                    index.append(i)
+            
+
+            for j in range(len(index)):
+                real_index = index[j]
+                blank_word = blank_word[:real_index*2] + user_input + blank_word[real_index*2 + 1:]
+            print(f"Good guess!, the letter {user_input} appears {len(index)} times in the word")
+            print(blank_word)
+            print(wrong_guesses)
+            #print(blank_word)
+
+        else:
+            number_of_guesses = number_of_guesses - 1
+            wrong_guesses = wrong_guesses + user_input + " "
+            print(f"Sorry!, the letter {user_input} does not exists in the word")
+            print(f"you can still have {number_of_guesses} wrong guesses")
+            
+            print(blank_word)
+            print(wrong_guesses)
+            
+
+            if len(wrong_guesses.replace(" ", "")) >= len(selected_word):
+                print("Sorry!, you have guessed too many wrong letters. The word was " + selected_word)
+                break
+
+        
         if blank_word.replace(" ", "") == selected_word:
             print("Congratulations!, You guessed the word!")
             break
-
+    #print(blank_word)
+    ask_if_again = input("Do you wanna play again? (y/n)")
+    if ask_if_again == "y":
+        main()
+    else:
+        print("Thanks for playing word guesser game")
+        print("Hope to see you again!")
 if __name__ == "__main__":
     main()
